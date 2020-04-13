f3b1892018aad6fc6133ba70f6c5b2c12d811708
Maciej Lis <mail@maciejlis.com>
2020-04-14 00:09:57 +0200

    Cleanup

--- Perceptron/Program.cs
+++ Perceptron/Program.cs
@@ -10,48 +10,46 @@ namespace Perceptron
         static double Sigmoid(double x) => 1 / (1 + Math.Exp(-x));
         static double SigmoidDerivative(double x) => x * (1 - x);
 
+        static int[][] trainingInputs = new int[][]
+        {
+            new [] { 0,0,1 },
+            new [] { 1,1,1 },
+            new [] { 1,0,1 },
+            new [] { 0,1,1 },
+        };
+        static int[] trainingOutputs = new[] { 0, 1, 1, 0 };
+        static double[] weights = new double[] { -0.16595599d, 0.44064899d, -0.99977125d };
+
         static void Main(string[] args)
         {
-            var training_inputs = new int[][]
-            {
-                new [] { 0,0,1 },
-                new [] { 1,1,1 },
-                new [] { 1,0,1 },
-                new [] { 0,1,1 },
-            };
-
-            var training_outputs = new[] { 0, 1, 1, 0 };
-
-            var synaptic_weights = new double[] { -0.16595599d, 0.44064899d, -0.99977125d }.ToList();
-
             // training network
             // iteration
             for (int i = 0; i < 1000; i++)
             {
                 // training set
-                for (int j = 0; j < training_inputs.Length; j++)
+                for (int j = 0; j < trainingInputs.Length; j++)
                 {
-                    var training_input = training_inputs[j];
-                    var training_output = training_outputs[j];
+                    var trainingInput = trainingInputs[j];
+                    var trainingOutput = trainingOutputs[j];
 
                     // calculate output
                     double sum = 0;
-                    for (int k = 0; k < training_input.Length; k++)
+                    for (int k = 0; k < trainingInput.Length; k++)
                     {
-                        sum += training_input[k] * synaptic_weights[k];
+                        sum += trainingInput[k] * weights[k];
                     }
                     var output = Sigmoid(sum);
 
                     // calculate error
-                    var error = training_output - output;
+                    var error = trainingOutput - output;
 
                     // calculate adjustment
                     var adjustment = error * SigmoidDerivative(output);
 
                     // set new weights
-                    for (int k = 0; k < training_input.Length; k++)
+                    for (int k = 0; k < trainingInput.Length; k++)
                     {
-                        synaptic_weights[k] += training_input[k] * adjustment;
+                        weights[k] += trainingInput[k] * adjustment;
                     }
                 }
             }
@@ -61,22 +59,22 @@ namespace Perceptron
             // test network
             while (true)
             {
-                Console.WriteLine("Enter 3 values followed by [enter] from set [0,1]");
+                Console.WriteLine("Enter 3 values followed by enter from set [0,1]");
                 var input1 = Double.Parse(Console.ReadLine());
                 var input2 = Double.Parse(Console.ReadLine());
                 var input3 = Double.Parse(Console.ReadLine());
 
-                var user_input = new double[] { input1, input2, input3 };
+                var userInput = new double[] { input1, input2, input3 };
 
                 // calculate output
                 double sum = 0;
-                for (int k = 0; k < user_input.Length; k++)
+                for (int k = 0; k < userInput.Length; k++)
                 {
-                    sum += user_input[k] * synaptic_weights[k];
+                    sum += userInput[k] * weights[k];
                 }
-                var user_output = Sigmoid(sum);
+                var userOutput = Sigmoid(sum);
 
-                Console.WriteLine("Output of network: " + user_output);
+                Console.WriteLine("Output of network: " + userOutput);
             }
         }
     }
